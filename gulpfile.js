3437e667404b0ac9939348534cd9ac49c48d7a7f
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-08 17:15:37 -0500

    Added first pass of 'write-files' task to Gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 // IMPORT MODULES
+var fs = require( 'fs' );
 var gulp = require( 'gulp' );
 
 
 // DEFINE TASKS
 gulp.task( 'default', function() {
 	console.log( 'INSIDE GULP TASK: `default`' );
+} );
+
+gulp.task( 'write-files', function() {
+	for ( var i = 0, x = 10; i < x; i++ ) {
+		fs.writeFile( __dirname + '/src/test-' + i + '.txt', 'Test 123\n', function( err ) {
+		} );
+	}
+
+	console.log( 'DONE WRITING FILES!' );
 } );
