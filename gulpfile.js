efe9872b84fbb7e3956dce72b09959386c4eebeb
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-08 17:23:40 -0500

    Updated 'write-files' task to call 'readFileSync' method. SYNCHRONOUS OPERATIONS 4LYFE.

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,12 @@ gulp.task( 'default', [ 'write-files', 'delete-files' ], function() {
 
 gulp.task( 'write-files', function() {
 	for ( var i = 0, x = 10; i < x; i++ ) {
-		fs.writeFile( __dirname + '/src/test-' + i + '.txt', 'Test 123\n', function( err ) {
+		fs.writeFileSync( __dirname + '/src/test-' + i + '.txt', 'Test 123\n', function( err ) {
 		} );
 	}
 
 	console.log( 'DONE WRITING FILES!' );
+	return;
 } );
 
 gulp.task( 'delete-files', function() {
